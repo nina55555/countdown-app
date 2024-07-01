6d562b4f4260bac6a7420f1c4e9a119fe77b3d17
Nina <morville5audrey@gmail.com>
2024-07-01 21:15:07 +0200

    first commit countdown set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [timerDays, setTimerDays]= useState ('00');
+  const [timerHours, setTimerHours]= useState ('00');
+  const [timerMinutes, setTimerMinutes]= useState ('00');
+  const [timerSeconds, setTimerSeconds]= useState ('00');
+
+  let interval = useRef();
+
+  const startTimer = () =>{
+    const countdownDate = new Date('July 16, 2024 10:43:00').getTime();
+  
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now ;
+
+      const days = Math.floor(distance / (1000 * 60 *60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 *60 * 24)/(1000 * 60 * 60)));
+      const minutes = Math.floor((distance % (1000 * 60 *60 ))/(1000 *60));
+      const seconds = Math.floor((distance % (1000 * 60 ))/ 1000);
+
+      if (distance < 0) {
+        //stop timer
+        clearInterval(interval.current);
+        //alert('hey you ! you win ! le vainqueur est le dernier a avoir encheri')
+
+      }else {
+        //update timer
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+
+    } , 1000);
+  };
+
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    
+    }
+      
+    
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+     <section className="timer-container">
+
+      <section className='timer'>
+
+        <div className='text'>
+          <h2>countdown timer</h2>
+          <p>countdown to a really special date, you will see</p> 
+        </div>
+
+        <div className='line'>
+
+         
+            <div className='box' ><p>{timerDays}</p></div>
+            <div className='type'><p>Days</p></div>
+         
+          <span>:</span>
+          
+            <div className='box' ><p>{timerHours} </p></div>
+            <div className='type'><p>Hours</p></div>
+         
+          <span>:</span>
+         
+            <div className='box' ><p>{timerMinutes} </p></div>
+            <div className='type'><p>Minutes</p></div>
+         
+          <span>:</span>
+          
+            <div className='box' ><p>{timerSeconds} </p></div>
+            <div className='type'><p>Seconds</p></div> 
+        </div>
+
+      </section>
+
+     </section>
   );
 }
 
